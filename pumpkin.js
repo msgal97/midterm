1c3f911957e828d9195c31978190d6d113e27c0b
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 20:08:56 -0500

    trial and error

--- pumpkin.js
+++ pumpkin.js
@@ -1,7 +1,4 @@
 function getRandomPosition(element) {
-  var x = document.body;
-  var y = document.body;
-  var randomX = Math.floor(Math.random()*x);
-  var randomY = Math.floor(Math.random()*y);
-  return[randomX, randomY];
+  var pick = Math.floor(Math.random()*x);
+  var pick = Math.floor(Math.random()*y);
 }
