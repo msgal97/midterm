667fe28dda071b9d5f3060b33b467cf6d02129dd
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 22:45:26 -0500

    js

--- pumpkin.js
+++ pumpkin.js
@@ -1,7 +1,9 @@
-function getRandomPosition(element) {
-	var x = document.body.offsetHeight-element.clientHeight;
-	var y = document.body.offsetWidth-element.clientWidth;
-	var randomX = Math.floor(Math.random()*x);
-	var randomY = Math.floor(Math.random()*y);
-	return [randomX,randomY];
+function createElement() {
+	var img = document.createElement('img');
+	img.setAttribute("style", "position:absolute;");
+	img.setAttribute("src", "message01.png");
+	document.body.appendChild(img);
+	var xy = getRandomPosition(img);
+	img.style.top = xy[0] + 'px';
+	img.style.left = xy[1] + 'px';
 }
