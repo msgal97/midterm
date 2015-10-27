87b3787ea99cd23a985fc9b33e546bab56c85b98
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 21:35:26 -0500

    js

--- pumpkin.js
+++ pumpkin.js
@@ -1,7 +1,4 @@
 function getElementbyId(myImg){
   var x = document.body.offsetHeight-element.clientHeight;
   var y = document.body.offsetWidth-element.clientWidth;
-  var randomX = Math.floor(Math.random()*x);
-  var randomY = Math.floor(Math.random()*y);
-  return [randomX,randomY];
 }
