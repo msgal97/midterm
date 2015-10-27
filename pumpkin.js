7ccd7f94b7730aa4a88dd6ad9d27f72cffcb3e2b
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 22:26:44 -0500

    final

--- pumpkin.js
+++ pumpkin.js
@@ -1,1 +1,7 @@
-
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
+}
