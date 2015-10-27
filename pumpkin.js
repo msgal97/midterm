5010e9df47edcbd3047f217fefd172898d9fe674
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 19:26:35 -0500

    updating

--- pumpkin.js
+++ pumpkin.js
@@ -1,4 +1,6 @@
 function getRandomPosition(element) {
+  var x = document.body;
+  var y = document.body;
   var randomX = Math.floor(Math.random()*x);
   var randomY = Math.floor(Math.random()*y);
   return[randomX, randomY];
