e5672a541193f0966100658d5f4b50d40225509f
msgal97 <klyoung2@mail.bradley.edu>
2015-10-26 22:51:46 -0500

    fingers crossed

--- pumpkin.js
+++ pumpkin.js
@@ -1,9 +1,7 @@
-function createElement() {
-	var img = document.createElement('img');
-	img.setAttribute("style", "position:absolute;");
-	img.setAttribute("src", "pickme1.png");
-	document.body.appendChild(img);
-	var xy = getRandomPosition(img);
-	img.style.top = xy[0] + 'px';
-	img.style.left = xy[1] + 'px';
+function getRandomPosition(element) {
+	var x = document.body.offsetHeight-element.clientHeight;
+	var y = document.body.offsetWidth-element.clientWidth;
+	var randomX = Math.floor(Math.random()*x);
+	var randomY = Math.floor(Math.random()*y);
+	return [randomX,randomY];
 }
